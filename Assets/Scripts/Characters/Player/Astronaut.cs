fd46a8ccbf715e578a59709adb4294b3c9444269
Adelsonjunior787 <junioradelson532@gmail.com>
2024-08-21 20:31:52 -0300

    added jump to prototype

--- Assets/Scripts/Characters/Player/Astronaut.cs
+++ Assets/Scripts/Characters/Player/Astronaut.cs
@@ -6,20 +6,43 @@ public class Astronaut : MonoBehaviour
 {
     public Rigidbody2D MyRigdbody;
 
-    public Vector2 Velocity;
+    public Vector2 Friction = new Vector2(-1, 0);
 
     public float Speed;
 
+    public float forceJump = 5;
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        HandleJump();
+        HandleMoviment();  
+    }
+
+    private void HandleMoviment()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
             MyRigdbody.velocity = new Vector2(-Speed, MyRigdbody.velocity.y);
 
         }
-        else if (Input.GetKeyDown (KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D))
         {
             MyRigdbody.velocity = new Vector2(Speed, MyRigdbody.velocity.y);
         }
+
+        if (MyRigdbody.velocity.x > 0)
+        {
+            MyRigdbody.velocity += Friction;
+        }
+        else if(MyRigdbody.velocity.x < 0)
+        {
+            MyRigdbody.velocity -= Friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MyRigdbody.velocity = Vector2.up * forceJump;
     }
 }
