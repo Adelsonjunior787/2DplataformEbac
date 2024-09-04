80a769da8e4605ef670335e065003f1dba3ed8c2
Adelsonjunior787 <junioradelson532@gmail.com>
2024-09-04 20:53:06 -0300

    Aula??? Esqueci

--- Assets/Scripts/Characters/Player/Astronaut.cs
+++ Assets/Scripts/Characters/Player/Astronaut.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Astronaut : MonoBehaviour
 {
     public Rigidbody2D MyRigdbody;
 
+    [Header("Speed Setup")]
     public Vector2 Friction = new Vector2(-1, 0);
-
     public float Speed;
-
     public float forceJump = 5;
-
     public float SpeedRun;
-
     private float _currentSpeed;
+
+    [Header("Animation Setup")]
+    public float JumpScaleY = 1.5f;
+    public float JumpScaleX = .7f;
+    public float AnimationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+
+
 
     private void Update()
     {
@@ -52,7 +58,21 @@ public class Astronaut : MonoBehaviour
     private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        { 
             MyRigdbody.velocity = Vector2.up * forceJump;
+            MyRigdbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(MyRigdbody.transform);
+
+            HandleScaleJump();
+        }
+        
+    }
+
+    private void HandleScaleJump()
+    {
+        MyRigdbody.transform.DOScaleY(JumpScaleY, AnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        MyRigdbody.transform.DOScaleX(JumpScaleX, AnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 
 
